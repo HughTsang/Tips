cf5188ef0444d5ff6fb403e04bcd3b17167c8c6d
ZHZ <ZHZ@163.com>
2016-08-15 17:20:32 +0800

    初始化项目

---
+++ Tips/Tips/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tips
+//
+//  Created by zenghz on 16/8/15.
+//  Copyright © 2016年 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tips/Tips/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Tips
+//
+//  Created by zenghz on 16/8/15.
+//  Copyright © 2016年 Personal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
